7f1f78214a002982ebbbca9c16c45ee24274d5eb
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-06-27 10:45:27 +0530

    node mysql

---
+++ Training/nodejs/backend/index.js
@@ -0,0 +1,26 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "newdb"
+}); 
+ 
+ 
+ 
+con.connect(function(err){
+    if(err)
+        console.log(err)
+    else{
+        con.query("SELECT * FROM tblusers",function(err, result){
+            if(err)
+                console.log(err)
+            else{
+                console.log(result)
+            }
+        })
+    }
+})
+
+ 
